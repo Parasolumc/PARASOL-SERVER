0228948c2afe9f189ccb7865d1ca53942fcb1e0c
Hyunjoon Choi <90085154+devholic22@users.noreply.github.com>
2023-07-05 16:13:51 +0900

    initial commit

---
+++ src/main/java/umc/parasol/ParasolApplication.java
@@ -0,0 +1,13 @@
+package umc.parasol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParasolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParasolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/umc/parasol/ParasolApplicationTests.java
@@ -0,0 +1,13 @@
+package umc.parasol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParasolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
