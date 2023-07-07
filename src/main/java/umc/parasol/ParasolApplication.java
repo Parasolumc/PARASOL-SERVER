c2effa9fdb3c439e88f55209198c186de26cccca
gyehwan24 <b99hwan@gmail.com>
2023-07-07 16:52:37 +0900

    [FEAT]: 초기 세팅

--- src/main/java/umc/parasol/ParasolApplication.java
+++ src/main/java/umc/parasol/ParasolApplication.java
@@ -2,8 +2,12 @@ package umc.parasol;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import umc.parasol.global.config.YamlPropertySourceFactory;
 
 @SpringBootApplication
+@PropertySource(value = {"classpath:database/application-database.yml"}, factory = YamlPropertySourceFactory.class)
+@PropertySource(value = {"classpath:auth/application-auth.yml"}, factory = YamlPropertySourceFactory.class)
 public class ParasolApplication {
 
 	public static void main(String[] args) {
