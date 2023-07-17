58dfcc0ea58485de81d0164459a8e3403d6ad817
Hyunjoon Choi <90085154+devholic22@users.noreply.github.com>
2023-07-17 16:24:59 +0900

    [FIX]: dto 값 로드 이슈 해결, BaseEntity null값 문제 해결

--- src/main/java/umc/parasol/ParasolApplication.java
+++ src/main/java/umc/parasol/ParasolApplication.java
@@ -3,9 +3,11 @@ package umc.parasol;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import umc.parasol.global.config.YamlPropertySourceFactory;
 
 @SpringBootApplication
+@EnableJpaAuditing
 @PropertySource(value = {"classpath:database/application-database.yml"}, factory = YamlPropertySourceFactory.class)
 @PropertySource(value = {"classpath:auth/application-auth.yml"}, factory = YamlPropertySourceFactory.class)
 public class ParasolApplication {
