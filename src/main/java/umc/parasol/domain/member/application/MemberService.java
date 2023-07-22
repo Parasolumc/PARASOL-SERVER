4222230b3808c9402cc136d6ee1b4be97c404c40
Gyehwan Baek <b99hwan@gmail.com>
2023-07-22 23:08:52 +0900

    [FEAT]: 비밀번호 변경, 회원 탈퇴 기초 작업

--- src/main/java/umc/parasol/domain/member/application/MemberService.java
+++ src/main/java/umc/parasol/domain/member/application/MemberService.java
@@ -1,4 +1,29 @@
 package umc.parasol.domain.member.application;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import umc.parasol.domain.member.domain.repository.MemberRepository;
+import umc.parasol.domain.member.dto.UpdatePwReq;
+import umc.parasol.global.config.security.token.UserPrincipal;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    // 비밀번호 변경
+    public void updatePassword(UpdatePwReq updatePwReq) {
+        // 1. 현재 비밀번호, 새 비밀번호, 새 비밀번호 재입력 ok -> 변경
+        // 2. 인증 후 비밀번호 재설정
+
+
+    }
+
+    // 회원 탈퇴
+    public void deleteMember(UserPrincipal userPrincipal) {
+        // 미반납 우산이 있거나, 미납된 연체료가 있는 회원은 탈퇴가 불가 (예외처리)
+    }
 }
