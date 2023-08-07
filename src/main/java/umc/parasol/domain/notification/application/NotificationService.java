a5eb491fc2563d3bd0e82b6aa62c3949e14541f8
chosolbee <sbcho0325@gmail.com>
2023-08-07 22:46:43 +0900

    [FEAT] : 알림 삭제 API 개발

--- src/main/java/umc/parasol/domain/notification/application/NotificationService.java
+++ src/main/java/umc/parasol/domain/notification/application/NotificationService.java
@@ -3,17 +3,13 @@ package umc.parasol.domain.notification.application;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.RequestBody;
 import umc.parasol.domain.member.domain.Member;
 import umc.parasol.domain.member.domain.repository.MemberRepository;
 import umc.parasol.domain.notification.domain.Notification;
 import umc.parasol.domain.notification.domain.repository.NotificationRepository;
 import umc.parasol.domain.notification.dto.NotificationRes;
-import umc.parasol.domain.shop.domain.Shop;
-import umc.parasol.domain.shop.domain.repository.ShopRepository;
 import umc.parasol.global.config.security.token.UserPrincipal;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -24,20 +20,11 @@ public class NotificationService {
     private final NotificationRepository notificationRepository;
     private final MemberRepository memberRepository;
 
-    private final ShopRepository shopRepository;
-
     // 유효한 사용자인지 체크하는 메서드
     private Member findValidMember(Long memberId) {
         return memberRepository.findById(memberId)
                 .orElseThrow(() -> new IllegalArgumentException("유저가 올바르지 않습니다."));
     }
-
-    // 유효한 매장인지 체크하는 메서드
-    public Shop findValidShop(Long shopId) {
-        return shopRepository.findById(shopId)
-                .orElseThrow(() -> new IllegalArgumentException("매장이 올바르지 않습니다."));
-    }
-
 
     // NotificationRes 응답을 생성해주는 메서드
     private NotificationRes createNotificationRes(Notification notification) {
@@ -51,6 +38,10 @@ public class NotificationService {
                 .build();
     }
 
+    /**
+     * 알림 조회
+     * @param user api 호출하는 사용자 객체
+     */
     @Transactional
     public List<NotificationRes> getAllNotifications(UserPrincipal user) {
         Member recipient = findValidMember(user.getId());
@@ -60,6 +51,22 @@ public class NotificationService {
         return notificationList.stream()
                 .map(this::createNotificationRes)
                 .toList();
+    }
+
+    /**
+     * 알림 삭제
+     * @param user api 호출하는 사용자 객체
+     * @param id 삭제할 알림 ID
+     */
+    @Transactional
+    public Object deleteNotification(UserPrincipal user, Long id) {
+        findValidMember(user.getId());
+        Notification notification = notificationRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("알림이 올바르지 않습니다."));
+
+        notificationRepository.delete(notification);
+
+        return true;
     }
 
 }
